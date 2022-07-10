b7396f08953424037864aadf4349cc175df16057
236630 <236630@edu.p.lodz.pl>
2022-07-10 22:23:59 +0200

    ac/ca

---
+++ ac_to_ca/player.py
@@ -0,0 +1,31 @@
+import numpy as np
+import soundcard as sc
+from scipy.io.wavfile import read
+
+
+class Player:
+    def __init__(self, sampling):
+        self.sampling = sampling
+        self.audio = sc.default_speaker()
+
+    def play_recorded(self, data: np.ndarray) -> str:
+        try:
+            self.audio.play(data, self.sampling)
+            return "Playing recorded audio."
+        except TypeError:
+            return "There is no recorded audio to play."
+
+    def play_saved(self, filename: str) -> str:
+        try:
+            data = read(filename)
+            sampling = data[0]
+            data = np.float64(data[1] / np.max(abs(data[1])))
+            channels = []
+            for i in range(len(data[0])):
+                channels.append(i)
+            self.audio.play(data, sampling, channels)
+            return "Saved audio has been played successfully."
+        except FileNotFoundError:
+            return "File with audio content does not exist."
+        except TypeError:
+            return "File with audio content is invalid."
