4e6f9dd29b74258422b39bc66dc4967386ae97e7
236630 <236630@edu.p.lodz.pl>
2022-07-11 14:53:59 +0200

    graphs

---
+++ bisection_euler/graphs.py
@@ -0,0 +1,70 @@
+from matplotlib import pyplot as plt
+
+
+def graph(func, p, k, wynik, met):
+
+    plt_precision = 0.0001
+    pocz = p
+    fx = [0.0] * int((abs(p) + k) / plt_precision)
+    fy = [0.0] * int((abs(p) + k) / plt_precision)
+    for i in range(len(fx)):
+        fx[i] = pocz
+        fy[i] = func(pocz)
+        pocz += plt_precision
+
+    min_ = min(abs(p), abs(k))
+
+    min_height = -min(fy) - 0.5
+    if min(fy) > -min_:
+        min_height = -min_
+    elif min(fy) < -3 * min_:
+        min_height = -3 * min_
+
+    max_height = max(fy) + 0.5
+    if max(fy) < min_:
+        max_height = min_
+    elif max(fy) > 3 * min_:
+        max_height = 3 * min_
+
+    name = "Metoda "
+    if met == 1:
+        name += "bisekcji"
+    else:
+        name += "siecznych"
+
+    fig = plt.figure(dpi=500)
+    ax = fig.add_subplot(1, 1, 1)
+
+    ax.spines['left'].set_position('zero')
+    ax.spines['bottom'].set_position('zero')
+
+    ax.spines['right'].set_color('none')
+    ax.spines['top'].set_color('none')
+
+    ax.xaxis.set_ticks_position('bottom')
+    ax.yaxis.set_ticks_position('left')
+
+    ax.annotate('x', xy=(1, 0), xycoords=('axes fraction', 'data'),
+                xytext=(7, 0), textcoords='offset points',
+                ha='left', va='center')
+
+    ax.annotate('y', xy=(0, 1), xycoords=('data', 'axes fraction'),
+                xytext=(0, 7), textcoords='offset points',
+                ha='center', va='bottom',)
+
+    ax.text(x=wynik + 0.3, y=0.3, s=f"x0 = {round(wynik, 3)}...", fontsize=6)
+
+    if p > 0:
+        plt.xlim(0, k)
+    elif k < 0:
+        plt.xlim(p, 0)
+    else:
+        plt.xlim(p, k)
+
+    plt.ylim(min_height, max_height)
+
+    plt.axis('scaled')
+    plt.scatter(wynik, func(wynik), color='red')
+    plt.plot(fx, fy, color='blue')
+    plt.savefig("jpg/" + name + ".jpg", dpi=500)
+    plt.show()
