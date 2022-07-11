63b099b0091d94afb0454822262b33be607e69c8
236630 <236630@edu.p.lodz.pl>
2022-07-11 13:11:35 +0200

    all models implemented in main.py

--- ac_to_ca/main.py
+++ ac_to_ca/main.py
@@ -1,16 +1,78 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import logging
+from player import Player
+from recorder import Recorder
+from time import sleep
 
 
-# Press the green button in the gutter to run the script.
+def int_choice(msg, cond=None):
+    while True:
+        try:
+            n = int(input(msg))
+            if cond is not None:
+                if n not in cond:
+                    raise ValueError
+            return n
+        except ValueError:
+            print("Try again!")
+
+
+def main():
+    sampling = abs(int_choice("Frequency of sampling [Hz]: "))
+    quantization = int_choice("Level of quantization [8, 16, 32]: ", [8, 16, 32])
+    print()
+
+    menu = ["Type 'r' to start recording", "Type 's' to save recorded audio",
+            "Type 'p' to play recorded audio", "Type 'z' to play saved audio",
+            "Type 'q' to quit the program", "Type 'h' to display all options"]
+
+    for line in menu:
+        print(line)
+
+    recorder = Recorder(sampling, quantization)
+    player = Player(sampling)
+    logging.basicConfig(format='%(asctime)s : %(message)s', datefmt='%H:%M:%S', level=logging.INFO)
+    msg = ''
+
+    while True:
+        opt = input("\nOption: ")
+        match opt:
+            case 'r':
+                recorder.clear_recorded_audio()
+                time = int_choice("Set recording time [s]: ")
+                logging.info("Recording is starting in: ")
+                sleep(0.1)
+                i = 3
+                while i > 0:
+                    print(f"{i}...")
+                    sleep(1)
+                    i -= 1
+                recorder.record(time)
+                logging.info("Recording is stopped.")
+
+            case 's':
+                msg = recorder.save("audio.wav")
+
+            case 'p':
+                msg = player.play_recorded(recorder.rec_audio)
+
+            case 'z':
+                msg = player.play_saved("audio.wav")
+
+            case 'q':
+                logging.info("Exiting the program.")
+                break
+
+            case 'h':
+                for line in menu:
+                    print(line)
+
+            case _:
+                logging.info("Unknown option.")
+
+        if opt in ['s', 'p', 'z']:
+            logging.info(msg)
+        sleep(0.1)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
