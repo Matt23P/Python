4e7878e4e78280b5d5af117c319938d5555c8077
Mateusz Przybyl <236630@edu.p.lodz.pl>
2022-10-22 02:24:49 +0200

    main.py properly working, project finished

--- gauss_elimination/main.py
+++ gauss_elimination/main.py
@@ -10,21 +10,29 @@ def gauss_elimination():
     match mode:
         case "determinate":
             start = time.time_ns()
-
+            x = methods.gaussian(x, y)
             total = (time.time_ns() - start) / 1000000
             print("Algorithm uptime: " + str(total) + "ns\n")
+            print("Result: ")
+            result = ""
+            for i in range(len(x)):
+                if i != 0:
+                    result += ", "
+                result += f'x{i + 1} = {x[i]}'
+            print(result)
             exit(0)
+
         case "indeterminate":
             print(colorama.Fore.LIGHTRED_EX +
                   "Matrix is indeterminate!"
                   + colorama.Style.RESET_ALL)
             exit(0)
+
         case "contradictory":
             print(colorama.Fore.LIGHTRED_EX +
                   "Matrix is contradictory!"
                   + colorama.Style.RESET_ALL)
             exit(0)
-
 
 
 if __name__ == '__main__':
