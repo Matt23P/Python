544d094437a0278d18a91f38b098186e779dbcfa
Mateusz Przybyl <236630@edu.p.lodz.pl>
2022-10-22 02:21:45 +0200

    added switch_rows, check_diagonal, gaussian methods to methods.py

--- gauss_elimination/methods.py
+++ gauss_elimination/methods.py
@@ -13,3 +13,38 @@ def check(x, y):
             return "determinate"
         elif rank_x < len(x[0]):
             return "indeterminate"
+
+
+def switch_rows(m, i, j):
+    temp = m[i].copy()
+    m[i] = m[j]
+    m[j] = temp
+
+
+def check_diagonal(a, b):
+    n = len(a)
+    for j in range(n):
+        max_el, index = a[0][j], 0
+        for i in range(1, n):
+            if abs(a[i][j]) > abs(max_el):
+                max_el, index = a[i][j], i
+        switch_rows(a, j, index)
+        switch_rows(b, j, index)
+
+
+def gaussian(a, b):
+    n = len(a)
+    check_diagonal(a, b)
+    for k in range(n - 1):
+        for i in range(k + 1, n):
+            m = a[i, k] / a[k, k]
+            for j in range(k, n):
+                a[i, j] -= m * a[k, j]
+            b[i] -= m * b[k]
+
+    x = np.zeros(n)
+    for k in range(n - 1, -1, -1):
+        rest = a[k, k + 1, n] @ x[k + 1:n]
+        x[k] = (b[k] - rest) / a[k, k]
+
+    return x
