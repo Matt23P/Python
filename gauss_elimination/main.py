c1ba184e454991a828fc2892ceaf5eefa3682464
Mateusz Przybyl <236630@edu.p.lodz.pl>
2022-10-22 02:12:58 +0200

    main.py match case added to determine the matrix type

--- gauss_elimination/main.py
+++ gauss_elimination/main.py
@@ -1,10 +1,31 @@
 import choice
 import time
+import methods
+import colorama
 
 
-def start():
+def gauss_elimination():
     x, y = choice.choose_a_file()
+    mode = methods.check(x, y)
+    match mode:
+        case "determinate":
+            start = time.time_ns()
+
+            total = (time.time_ns() - start) / 1000000
+            print("Algorithm uptime: " + str(total) + "ns\n")
+            exit(0)
+        case "indeterminate":
+            print(colorama.Fore.LIGHTRED_EX +
+                  "Matrix is indeterminate!"
+                  + colorama.Style.RESET_ALL)
+            exit(0)
+        case "contradictory":
+            print(colorama.Fore.LIGHTRED_EX +
+                  "Matrix is contradictory!"
+                  + colorama.Style.RESET_ALL)
+            exit(0)
+
 
 
 if __name__ == '__main__':
-    start()
+    gauss_elimination()
